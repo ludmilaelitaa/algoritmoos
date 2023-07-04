2fa054bd20944f837ffa7dad31390f91f9bba652
ludmilaelitaa <138604148+ludmilaelitaa@users.noreply.github.com>
2023-07-04 20:27:48 -0300

    Create l4q2.cpp

---
+++ l4q2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+using namespace std;
+int main() {
+  char q2[50];
+  int i;
+  fgets(q2,50,stdin);
+  for(i=0;i<4;i++){
+    cout << q2[i];
+  }
+}
