2c8b32f497642cfce66667ecca9a6db2f719bd0b
ludmilaelitaa <138604148+ludmilaelitaa@users.noreply.github.com>
2023-07-04 20:46:28 -0300

    Create l4q3.cpp

---
+++ l4q3.cpp
@@ -0,0 +1,16 @@
+nome: ludmila elita
+turma if23
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+int main() {
+  string q3;
+  getline(cin,q3);
+  int v = 0;
+    for (int i = 0; i < q3.size(); i++) {
+        v++;
+    }
+  cout << v;
+    
+}
