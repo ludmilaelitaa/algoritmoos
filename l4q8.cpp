fc8220461454183847bd91daf984db50e1b4c30f
ludmilaelitaa <138604148+ludmilaelitaa@users.noreply.github.com>
2023-07-04 22:21:08 -0300

    Update l4q8.cpp

--- l4q8.cpp
+++ l4q8.cpp
@@ -1,13 +1,18 @@
-/*nome:ludmila elita, turma if23*/
+/*nome:ludmila elita
+turma if23
+q: Construa um programa que leia duas strings do teclado. Imprima uma mensagem informando
+se a segunda string lida está contida dentro da primeira. (estude a função strstr() que realiza
+esta tarefa)
+*/
 #include <iostream>
 #include <cstring>
 using namespace std;
 
 int main(){
-    char num1[100];
-    char num2[100];
-    cin.getline(num1, 100);
-    cin.getline(num2, 100);
+    char num1[40];
+    char num2[40];
+    cin.getline(num1, 40);
+    cin.getline(num2, 40);
 
     char* resultado = strstr(num1, num2);
 
